cd0810a59b64050e3ff5bdf425dd056e0e154973
Sumanto Banerjee <sumanto.banerjee08@Sumantos-MacBook-Pro-2.local>
2022-08-22 21:46:29 +0530

    Rendering a list

--- src/Cart.js
+++ src/Cart.js
@@ -4,11 +4,15 @@ import CartItem from './CartItem';
 // create class component
 class Cart extends React.Component {
     render(){
+        const arr = [1,2,3,4,5];
         return (
             <div className="cart">
+                {/* <CartItem />
                 <CartItem />
-                <CartItem />
-                <CartItem />
+                <CartItem /> */}
+                {arr.map((item) => {
+                    return item + 5
+                })}
             </div>
         );
     }
