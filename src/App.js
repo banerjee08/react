aa5206cdf4b399d70eb1a5d62f35bdbbc7781b2d
Sumanto Banerjee <sbanerjee@Sumantos-MacBook-Pro.local>
2022-08-23 00:29:29 +0530

    Adding the Navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 // import './App.css';
 import React from 'react';
-import Cart from './Cart'
+import Cart from './Cart';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Cart</h1>
       <Cart />
     </div>
